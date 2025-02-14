9d0a39d903cf5af4996eb76f0dc6022b3acc7838
Santiago Cuervo <santic9999@gmail.com>
2025-02-14 01:09:42 -0500

    Inyeccion de Dependencias

--- AppTaxi/Program.cs
+++ AppTaxi/Program.cs
@@ -1,8 +1,16 @@
+using AppTaxi.Servicios;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<I_Conductor>();
+builder.Services.AddScoped<I_Empresa>();
+builder.Services.AddScoped<I_Horario>();
+builder.Services.AddScoped<I_Invitado>();
+builder.Services.AddScoped<I_Propietario>();
+builder.Services.AddScoped<I_Rol>();
+builder.Services.AddScoped<I_Usuario>();
+builder.Services.AddScoped<I_Vehiculo>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
