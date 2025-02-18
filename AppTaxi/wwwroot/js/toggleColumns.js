90e3ac82168a017ed9934ad81b9214a62b71952a
Santiago Cuervo <santic9999@gmail.com>
2025-02-18 00:06:07 -0500

    Funcion de esconder Columnas de Tablas

---
+++ AppTaxi/wwwroot/js/toggleColumns.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $(".column-toggle").change(function () {
+        var columnClass = ".col-" + $(this).val();
+        if ($(this).is(":checked")) {
+            $(columnClass).show();  // Muestra la columna
+        } else {
+            $(columnClass).hide();  // Oculta la columna
+        }
+    });
+
+    // Ocultar columnas al cargar la página si los checkboxes están desmarcados
+    $(".column-toggle").each(function () {
+        var columnClass = ".col-" + $(this).val();
+        if (!$(this).is(":checked")) {
+            $(columnClass).hide();
+        }
+    });
+});
