f3b103c2a76ac46337ebd7fddcd824f56aea2652
Santiago Cuervo <santic9999@gmail.com>
2025-03-15 21:43:26 -0500

    Transaccion

--- AppTaxi/Servicios/I_Transaccion.cs
+++ AppTaxi/Servicios/I_Transaccion.cs
@@ -5,9 +5,9 @@ namespace AppTaxi.Servicios
     public interface I_Transaccion
     {
         Task<List<Transaccion>> Lista(Login login);
-        Task<Transaccion> Obtener(int IdAccion, Login login);
+        Task<Transaccion> Obtener(int IdTransaccion, Login login);
         Task<bool> Guardar(Transaccion transaccion, Login login);
         Task<bool> Editar(Transaccion transaccion, Login login);
-        Task<bool> Eliminar(int IdAccion, Login login);
+        Task<bool> Eliminar(int IdTransaccion, Login login);
     }
 }
